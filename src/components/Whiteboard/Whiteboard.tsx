725847b493fb06e1329baa8ac9d85f7457426a5f
Shagun Mistry <shagun.mistry@hotmail.com>
2024-01-07 12:22:37 -0500

    Update to dark theme for white board

--- src/components/Whiteboard/Whiteboard.tsx
+++ src/components/Whiteboard/Whiteboard.tsx
@@ -71,6 +71,11 @@ export default function ({ inputJson }: { inputJson: string }) {
 
           // Zoom to fit the diagram
           editor?.zoomToFit({ duration: 200 })
+
+          editor?.user.updateUserPreferences({
+            isDarkMode: true,
+            isSnapMode: true,
+          })
         }}
       />
       <ErrorDialog
