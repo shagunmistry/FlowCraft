7a6a59d70527776c72d907ad1bc5551645b80b24
Shagun Mistry <shagun.mistry@hotmail.com>
2024-03-30 22:13:33 -0400

    Add in help button on profile dropdown

--- src/components/Dashboard/Navbar.dashboard.tsx
+++ src/components/Dashboard/Navbar.dashboard.tsx
@@ -143,32 +143,19 @@ export default function DashboardNavbar() {
                       leaveTo="transform opacity-0 scale-95"
                     >
                       <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {/* <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={cn(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700',
-                              )}
-                            >
-                              Your Profile
-                            </a>
-                          )}
-                        </Menu.Item>
                         <Menu.Item>
                           {({ active }) => (
-                            <a
-                              href="#"
+                            <Link
+                              href="/support"
                               className={cn(
                                 active ? 'bg-gray-100' : '',
                                 'block px-4 py-2 text-sm text-gray-700',
                               )}
                             >
-                              Settings
-                            </a>
+                              Help
+                            </Link>
                           )}
-                        </Menu.Item> */}
+                        </Menu.Item>
                         <Menu.Item>
                           {({ active }) => (
                             <Link
