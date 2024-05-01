904137db2dbd4c43df78affb865278a9ab4c7136
Shagun Mistry <shagun.mistry@hotmail.com>
2024-04-30 22:17:03 -0400

    Update landing page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -7,6 +7,7 @@ import {
 } from '@/lib/supabase'
 
 import MainLanding from '@/components/Landing/Main.landing'
+import { Metadata } from 'next'
 
 const getEmbeddings = async () => {
   const reactFlowCodeDocs = await getReactCodeFlowJSONFile()
@@ -48,6 +49,12 @@ const getEmbeddings = async () => {
       }),
     }
   }
+}
+
+export const metadata: Metadata = {
+  title: 'FlowCraft',
+  description:
+    'FlowCraft is a diagramming tool that helps you create flowcharts, process maps, and system diagrams with AI and with ease. Some diagram types include Flowcharts, sequence diagrams, User journey maps, mind maps, knowledge graphs, and more.',
 }
 
 export default async function Home() {
--- src/components/Landing/Main.landing.tsx
+++ src/components/Landing/Main.landing.tsx
@@ -113,8 +113,143 @@ export default function MainLanding() {
       </Popover>
 
       <main>
-        <div className="bg-gray-100 pt-10 sm:pt-16 lg:overflow-hidden lg:pb-14 lg:pt-8">
-          <div className="relative mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="bg-gray-100 lg:overflow-hidden lg:pb-14">
+          <main>
+            <div className="relative isolate">
+              <svg
+                className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-pink-300 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]"
+                aria-hidden="true"
+              >
+                <defs>
+                  <pattern
+                    id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84"
+                    width={200}
+                    height={200}
+                    x="50%"
+                    y={-1}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <path d="M.5 200V.5H200" fill="none" />
+                  </pattern>
+                </defs>
+                <svg x="50%" y={-1} className="overflow-visible fill-pink-300">
+                  <path
+                    d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+                    strokeWidth={0}
+                  />
+                </svg>
+                <rect
+                  width="100%"
+                  height="100%"
+                  strokeWidth={0}
+                  fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)"
+                />
+              </svg>
+              <div
+                className="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48"
+                aria-hidden="true"
+              >
+                <div
+                  className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+                  style={{
+                    clipPath:
+                      'polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)',
+                  }}
+                />
+              </div>
+              <div className="overflow-hidden">
+                <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-16">
+                  <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+                    <div className="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
+                      <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
+                        Create stunning diagrams, charts, and sketches
+                        <span className="block text-pink-500">
+                          with AI in seconds.
+                        </span>
+                      </h1>
+                      <p className="mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none"></p>
+                      <div className="mt-10 flex items-center gap-x-6">
+                        <Link
+                          className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-pink-500 px-8 py-3 font-medium text-gray-500 text-indigo-600 text-white transition-all duration-300 ease-in-out hover:scale-125 hover:bg-indigo-500 sm:inline-flex sm:w-auto sm:items-center sm:px-6"
+                          href={authenticated ? '/dashboard' : '/login'}
+                        >
+                          {authenticated
+                            ? 'Go to Dashboard'
+                            : 'Get Started For Free'}
+                        </Link>
+                        <Link
+                          className="inline-flex items-center gap-x-1.5 rounded-md bg-blue-500 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm transition-all duration-300 ease-in-out hover:scale-105 hover:bg-blue-500 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                          href="https://marketplace.visualstudio.com/items?itemName=FlowCraft.flowcraft"
+                        >
+                          <VSCodeIcon />
+                          Download VS Code Extension
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+                      <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
+                        <div className="relative">
+                          <Image
+                            src="https://firebasestorage.googleapis.com/v0/b/shagunresume.appspot.com/o/FlowCraft%2FDashboard%20-%20FlowCraft.jpeg?alt=media&token=2ba5c2db-a62c-48e5-ba05-c50c18e97865"
+                            alt=""
+                            className="w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                            width={500}
+                            height={600}
+                          />
+                          <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                        </div>
+                      </div>
+                      <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+                        <div className="relative">
+                          <Image
+                            src="https://firebasestorage.googleapis.com/v0/b/shagunresume.appspot.com/o/FlowCraft%2FFlowCraft%20Mindmaps.png?alt=media&token=e9b039f1-539d-41aa-beac-42616ca2a117"
+                            alt=""
+                            className="w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                            width={500}
+                            height={600}
+                          />
+                          <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                        </div>
+                        <div className="relative">
+                          <Image
+                            src="https://firebasestorage.googleapis.com/v0/b/shagunresume.appspot.com/o/FlowCraft%2FHowDoesPhotoSynthesisWork.png?alt=media&token=85d034ab-1cef-4dca-8122-1073857a8c81"
+                            alt=""
+                            className="w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                            width={500}
+                            height={600}
+                          />
+                          <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                        </div>
+                      </div>
+                      <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
+                        <div className="relative">
+                          <Image
+                            src="https://firebasestorage.googleapis.com/v0/b/shagunresume.appspot.com/o/FlowCraft%2FMermaidDiagrams%2FClassDiagram.png?alt=media&token=e0838b08-21f8-42fa-9e04-ccb100313673"
+                            alt=""
+                            className="w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                            width={500}
+                            height={600}
+                          />
+                          <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                        </div>
+                        <div className="relative">
+                          <Image
+                            src="https://firebasestorage.googleapis.com/v0/b/shagunresume.appspot.com/o/FlowCraft%2FMermaidDiagrams%2FGitDiagram.png?alt=media&token=8a41a1a9-eb24-485b-ade3-e64d57ae0f33"
+                            alt=""
+                            className="w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                            width={500}
+                            height={600}
+                          />
+                          <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
+          {/* <div className="relative mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="lg:grid lg:grid-cols-2 lg:gap-8">
               <div className="mx-auto max-w-md px-6 sm:max-w-2xl sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
                 <div className="lg:py-24">
@@ -185,7 +320,7 @@ export default function MainLanding() {
                 />
               </div>
             </div>
-          </div>
+          </div> */}
           {/** Stats Section */}
           <div className="py-24 sm:py-12">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
