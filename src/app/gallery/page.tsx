20eaf35040b8175e8fe7bf10d57805de69ad93e0
elderNova126 <que.quao126@gmail.com>
2025-04-24 22:09:29 +0700

    add unauthebticate gallery & issue 23

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import FAQs from '@/components/FAQ'
+import Navbar from '@/components/Navbar'
+import PublicGallery from '@/components/Gallery/Gallery'
+import { useSearchParams } from 'next/navigation'
+
+export default function Pricing() {
+  const searchParams = useSearchParams()
+  const sourcePage = searchParams.get('sourcePage') as
+    | 'landing'
+    | 'dashboard'
+    | 'mermaid'
+    | 'chart'
+  return (
+    <>
+      <Navbar />
+      <PublicGallery />
+      <FAQs />
+    </>
+  )
+}
