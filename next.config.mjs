a357178b0b40398655da78aff4b2dbe704b86354
elderNova126 <que.quao126@gmail.com>
2025-05-12 22:45:03 +0700

    fix build error.

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  transpilePackages: ['mermaid', 'cytoscape'],
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Environment variables
   env: {
